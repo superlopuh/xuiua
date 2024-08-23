c9f184d5533fed2b1d6f9df22a603769ed902a31
Sasha Lopoukhine <superlopuh@gmail.com>
2024-08-23 14:34:11 +0100

    add reduce and yield ops to the dialect

--- tests/filecheck/dialect.mlir
+++ tests/filecheck/dialect.mlir
@@ -11,3 +11,17 @@
 
 // CHECK-NEXT:  %multiply = "uiua.multiply"(%t, %t) : (tensor<*xf64>, tensor<*xf64>) -> tensor<*xf64>
 %multiply = "uiua.multiply"(%t, %t) : (tensor<*xf64>, tensor<*xf64>) -> tensor<*xf64>
+
+// CHECK-NEXT:  %arg_reduce = "test.op"() : () -> tensor<2x3x4xf64>
+%arg_reduce = "test.op"() : () -> tensor<2x3x4xf64>
+
+// CHECK-NEXT:  %reduce = "uiua.reduce"(%arg_reduce) ({
+// CHECK-NEXT:  ^0(%acc_reduce : tensor<5x6xf64>, %val_reduce : tensor<3x4xf64>):
+%reduce = "uiua.reduce"(%arg_reduce) ({
+^0(%acc_reduce : tensor<5x6xf64>, %val_reduce : tensor<3x4xf64>):
+    // CHECK-NEXT:  %next_reduce = "test.op"(%acc_reduce, %val_reduce) : (tensor<5x6xf64>, tensor<3x4xf64>) -> tensor<5x6xf64>
+    %next_reduce = "test.op"(%acc_reduce, %val_reduce) : (tensor<5x6xf64>, tensor<3x4xf64>) -> tensor<5x6xf64>
+    // CHECK-NEXT:  "uiua.yield"(%next_reduce) : (tensor<5x6xf64>) -> ()
+    "uiua.yield"(%next_reduce) : (tensor<5x6xf64>) -> ()
+// CHECK-NEXT:  }) : (tensor<2x3x4xf64>) -> tensor<5x6xf64>
+}) : (tensor<2x3x4xf64>) -> tensor<5x6xf64>
--- xuiua/dialect.py
+++ xuiua/dialect.py
@@ -6,15 +6,18 @@ from xdsl.dialects.builtin import (
     UnrankedTensorType,
     f64,
 )
-from xdsl.ir import Attribute, Dialect, SSAValue
+from xdsl.ir import Attribute, Dialect, Region, SSAValue, VerifyException
 from xdsl.irdl import (
     base,
     irdl_op_definition,
     IRDLOperation,
     operand_def,
+    region_def,
     result_def,
+    var_operand_def,
 )
-from xdsl.traits import HasShapeInferencePatternsTrait, Pure
+from xdsl.traits import HasParent, HasShapeInferencePatternsTrait, Pure
+from xdsl.utils.isattr import isattr
 
 TI32 = TensorType[I32]
 UTI32 = UnrankedTensorType[I32]
@@ -24,8 +27,8 @@ TF64 = TensorType[Float64Type]
 UTF64 = UnrankedTensorType[Float64Type]
 utf64 = UTF64(f64)
 
-TT = TI32 | TF32
-TTConstr = base(TI32) | base(TF32)
+TT = TI32 | TF32 | TF64
+TTConstr = base(TI32) | base(TF32) | base(TF64)
 UTT = UTI32 | UTF32 | UTF64
 UTTConstr = base(UTI32) | base(UTF32) | base(UTF64)
 UIUATensorType = TT | UTT
@@ -137,11 +140,76 @@ class MultiplyOp(IRDLOperation):
         super().__init__(operands=(lhs, rhs), result_types=(result_type,))
 
 
+@irdl_op_definition
+class ReduceOp(IRDLOperation):
+    """
+    Reduces a value left to right pairwise with a specified transform.
+
+    https://www.uiua.org/docs/reduce
+    """
+
+    name = "uiua.reduce"
+
+    arg = operand_def(UIUATensorConstr)
+    res = result_def(UIUATensorConstr)
+    body = region_def("single_block")
+
+    traits = frozenset((Pure(),))
+
+    def __init__(self, arg: SSAValue, result_type: Attribute, body: Region):
+        super().__init__(operands=(arg), result_types=(result_type,), regions=(body,))
+
+    def verify_(self) -> None:
+        args = self.body.block.args
+        if len(args) != 2:
+            raise VerifyException(
+                f"Invalid number of operands in reduce region: {len(args)}"
+            )
+
+        acc, val = args
+
+        # The first arg of the region is the accumulator
+        # It should have the same type as the result
+        if acc.type != self.res.type:
+            raise VerifyException(
+                f"Mismatching types for reduction accumulator: {acc.type} != {self.res.type}"
+            )
+
+        # The second arg of the region is the iteration element
+        # It should have one fewer dimension than the arg
+        if isattr(self.arg.type, TTConstr) and isattr(val.type, TTConstr):
+            arg_shape = self.arg.type.get_shape()
+            val_shape = val.type.get_shape()
+
+            if len(arg_shape) != (len(val_shape) + 1) or arg_shape[1:] != val_shape:
+                raise VerifyException(
+                    f"Mismatching shapes for reduction value and operand: {arg_shape} != {val_shape}"
+                )
+
+
+@irdl_op_definition
+class YieldOp(IRDLOperation):
+    """
+    Yields a reduction value.
+    """
+
+    name = "uiua.yield"
+
+    arg = var_operand_def(UIUATensorConstr)
+
+    traits = frozenset((lambda: HasParent(ReduceOp),))
+
+    def __init__(self, *args: SSAValue):
+        super().__init__(operands=(args))
+
+
 UIUA = Dialect(
     "uiua",
     [
         AddOp,
         CastOp,
         MultiplyOp,
+        ReduceOp,
+        YieldOp,
     ],
 )
