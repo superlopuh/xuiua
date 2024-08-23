a3ee9f44b7545d6a1ae128b0f0614dc7e1a7d4c2
Sasha Lopoukhine <superlopuh@gmail.com>
2024-08-23 22:51:06 +0100

    move things to compile.py

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -1,59 +1,8 @@
-import jax
 import jax.numpy as jnp
-from jax import config
 
 
-from xdsl.backend.jax_executable import JaxExecutable
-from xdsl.dialects.builtin import ModuleOp
-from xdsl.dialects.func import FuncOp
-from xdsl.traits import SymbolTable
-from xdsl.transforms import shape_inference
-
-from xuiua.frontend.ir_gen import build_module
-from xuiua.frontend.parser import Parser
-from xuiua.passes import remove_casts, convert_uiua_to_stablehlo
-from xuiua.passes.add_shapes import add_shapes
-from xuiua.compile import get_ctx
+from xuiua.compile import a, run
 import numpy as np
-
-config.update("jax_enable_x64", True)
-
-
-def a(els: tuple[float, ...]) -> jax.Array:
-    floats = tuple(float(el) for el in els)
-    return jnp.array(np.array(floats))
-
-
-def build_expr_module(expr: str) -> ModuleOp:
-    parser = Parser("main ← " + expr)
-    items = parser.parse_items()
-    module = build_module(items)
-    return module
-
-
-# shape-inference,remove-casts,convert-uiua-to-stablehlo
-SHAPED_PIPELINE = [
-    shape_inference.ShapeInferencePass(),
-    remove_casts.RemoveCastsPass(),
-    convert_uiua_to_stablehlo.ConvertUiuaToStableHLOPass(),
-]
-
-
-def run(expr: str, inputs: tuple[jax.Array, ...]) -> tuple[jax.Array, ...]:
-    module = build_expr_module(expr)
-    shapes = tuple(i.shape for i in inputs)
-    main_op = SymbolTable.lookup_symbol(module, "main")
-    assert isinstance(main_op, FuncOp)
-    add_shapes(main_op, shapes)
-    ctx = get_ctx()
-    for p in SHAPED_PIPELINE:
-        p.apply(ctx, module)
-
-    jax_executable = JaxExecutable.compile(module)
-
-    result = jax_executable.execute(inputs)
-
-    return tuple(result)
 
 
 def test_compile_add():
@@ -75,7 +24,7 @@ def test_dtype():
     assert my_jax_array.dtype == np.float64
 
 
-def test_compile_add():
+def test_compile_sum():
     A = a((2, 3, 4, 4.5, 5.5, 6.5)).reshape((2, 3))
     B = a((6.5, 8.5, 10.5))
     res0 = A.sum(axis=0)
--- xuiua/compile.py
+++ xuiua/compile.py
@@ -6,6 +6,23 @@ from xdsl.dialects import test
 
 from xuiua.dialect import UIUA
 
+import jax
+import jax.numpy as jnp
+from jax import config
+
+
+from xdsl.backend.jax_executable import JaxExecutable
+from xdsl.dialects.builtin import ModuleOp
+from xdsl.dialects.func import FuncOp
+from xdsl.traits import SymbolTable
+from xdsl.transforms import shape_inference
+
+from xuiua.frontend.ir_gen import build_module
+from xuiua.frontend.parser import Parser
+from xuiua.passes import remove_casts, convert_uiua_to_stablehlo
+from xuiua.passes.add_shapes import add_shapes
+import numpy as np
+
 
 def get_ctx() -> MLContext:
     ctx = MLContext()
@@ -14,3 +31,43 @@ def get_ctx() -> MLContext:
     ctx.register_dialect("uiua", lambda: UIUA)
     ctx.register_dialect("test", lambda: test.Test)
     return ctx
+
+
+config.update("jax_enable_x64", True)
+
+
+def a(els: tuple[float, ...]) -> jax.Array:
+    floats = tuple(float(el) for el in els)
+    return jnp.array(np.array(floats))
+
+
+def build_expr_module(expr: str) -> ModuleOp:
+    parser = Parser("main ← " + expr)
+    items = parser.parse_items()
+    module = build_module(items)
+    return module
+
+
+# shape-inference,remove-casts,convert-uiua-to-stablehlo
+SHAPED_PIPELINE = [
+    shape_inference.ShapeInferencePass(),
+    remove_casts.RemoveCastsPass(),
+    convert_uiua_to_stablehlo.ConvertUiuaToStableHLOPass(),
+]
+
+
+def run(expr: str, inputs: tuple[jax.Array, ...]) -> tuple[jax.Array, ...]:
+    module = build_expr_module(expr)
+    shapes = tuple(i.shape for i in inputs)
+    main_op = SymbolTable.lookup_symbol(module, "main")
+    assert isinstance(main_op, FuncOp)
+    add_shapes(main_op, shapes)
+    ctx = get_ctx()
+    for p in SHAPED_PIPELINE:
+        p.apply(ctx, module)
+
+    jax_executable = JaxExecutable.compile(module)
+
+    result = jax_executable.execute(inputs)
+
+    return tuple(result)
